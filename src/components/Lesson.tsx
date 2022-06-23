aedc53043be71c648372aa8e5038b3f0e1b9522e
Alisson Silva <alissonjosef@gmail.com>
2022-06-23 09:34:41 -0300

    adjust screen video

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock} from 'phosphor-react'
 import {isPast, format} from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom'
 
 interface LessonProps{
     availableAt: Date
@@ -10,19 +11,19 @@ interface LessonProps{
 }
 
 export function Lesson(props: LessonProps) {
-    const isLasseonAvailable = isPast(props.availableAt)
+    const isLessonAvailable = isPast(props.availableAt)
     const availableDateFormatted = format(props.availableAt,"EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
         locale:ptBR,
     })
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className='group'>
       <samp className="text-gray-300">
         {availableDateFormatted}
         </samp>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
-            {isLasseonAvailable ? (
+            {isLessonAvailable ? (
                 <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
             <CheckCircle  size={20}/>
             Conteúdo liberado
@@ -43,6 +44,6 @@ export function Lesson(props: LessonProps) {
             {props.title}
             </strong>
       </div>
-    </a>
+    </Link>
   );
 }
